ced0654603f2065b2214bf3a23296db53c536f08
Joachim Nilsson <troglobit@gmail.com>
2018-08-04 13:58:11 +0200

    Minor style fixes
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --- Redshift for Awesome WM
--- author: Ryan Young <ryan .ry. young@gmail. com> (omit spaces)
+-- Copyright (c) 2013  Ryan Young <ryan .ry. young@gmail. com> (omit spaces)
 --
--- https://github.com/YoRyan/awesome-redshift
+-- https://github.com/troglobit/awesome-redshift
 --
 
 -- standard libraries
@@ -10,21 +10,18 @@ local awful = require("awful")
 -- variables
 local redshift = {}
 redshift.redshift = "/usr/bin/redshift"    -- binary path
-redshift.method = "randr"                  -- randr or vidmode
-redshift.options = ""                      -- additional redshift command options
-redshift.state = 1                         -- 1 for screen dimming, 0 for none
-redshift.timer = timer({ timeout = 60 })
+redshift.method   = "randr"                -- randr or vidmode
+redshift.options  = ""                     -- additional redshift command options
+redshift.state    = 1                      -- 1 for screen dimming, 0 for none
+redshift.timer    = timer({ timeout = 60 })
 
 -- functions
-redshift.dim = function()
-    if redshift.method == "randr"
-    then
+function redshift.dim()
+    if redshift.method == "randr" then
         awful.util.spawn(redshift.redshift .. " -m randr -o " .. redshift.options)
-    elseif redshift.method == "vidmode"
-    then
+    elseif redshift.method == "vidmode" then
         local screens = screen.count()
-        for i = 0, screens - 1
-        do
+        for i = 0, screens - 1 do
             awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i ..
                              " -o " .. redshift.options)
         end
@@ -32,16 +29,14 @@ redshift.dim = function()
     redshift.state = 1
     redshift.timer:start()
 end
+
 redshift.timer:connect_signal("timeout", redshift.dim)
-redshift.undim = function()
-    if redshift.method == "randr"
-    then
+function redshift.undim()
+    if redshift.method == "randr" then
         awful.util.spawn(redshift.redshift .. " -m randr -x " .. redshift.options)
-    elseif redshift.method == "vidmode"
-    then
+    elseif redshift.method == "vidmode" then
         local screens = screen.count()
-        for i = 0, screens - 1
-        do
+        for i = 0, screens - 1 do
             awful.util.spawn(redshift.redshift .. " -m vidmode:screen=" .. i ..
                              " -x " .. redshift.options)
         end
@@ -49,17 +44,17 @@ redshift.undim = function()
     redshift.state = 0
     redshift.timer:stop()
 end
-redshift.toggle = function()
-    if redshift.state == 1
-    then
+
+function redshift.toggle()
+    if redshift.state == 1 then
         redshift.undim()
     else
         redshift.dim()
     end
 end
-redshift.init = function(initState)
-    if initState == 1
-    then
+
+function redshift.init(state)
+    if state == 1 then
         redshift.dim()
     else
         redshift.undim()
